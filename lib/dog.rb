461682d8f1a54aa554ca4055afac0039288c1258
a lan <bluenowte97@gmail.com>
2018-11-11 14:59:13 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+
+
+#name equals
+def name=(the_dog_name)
+  @name = the_dog_name
+end  
+
+#this is just reading it, it is name ,not name = which can reassign
+def name
+  @name
+end
+
+
+
+  
+
+
+
+
+
+
+end
+
+  
