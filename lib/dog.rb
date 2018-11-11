1f6efa4fd2ad4f2df5ebe70c7f9e5557704215f6
a lan <bluenowte97@gmail.com>
2018-11-11 15:14:17 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+  #name equals
+  def name=(the_dog_name)
+    @name = the_dog_name
+  end
+
+  #this is just reading it, it is name ,not name = which can reassign
+  def name
+    @name
+  end
+
+  def breed=(the_dog_breed)
+   @breed = the_dog_breed
+ end
+
+  def breed
+    @breed
+  end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(the_name_of_person)
+    @name = the_name_of_person
+  end
+
+  def name
+    @name
+  end
+
+  def job=(the_job_of_person)
+    @job = the_job_of_person
+  end
+
+  def job
+    @job
+  end
+
+end
